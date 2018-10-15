00e780e11db47aacc56ddc4fe05532795b712101
Moncer Ahmed <ahmed.moncer@esprit.tn>
2018-10-15 22:26:52 +0100

    com 2

--- app/src/main/java/com/tn/ahmed/ic/MainActivity.java
+++ app/src/main/java/com/tn/ahmed/ic/MainActivity.java
@@ -2,12 +2,15 @@ package com.tn.ahmed.ic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+TextView txt ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txt=(TextView)findViewById(R.id.txt);
+        txt.setText("Yoyo");
     }
 }
